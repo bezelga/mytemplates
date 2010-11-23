b2b7968e46f2f713b09bd23364594508aa9bfc60
Fabiano Beselga <fabianobeselga@gmail.com>
2010-11-23 08:26:04 -0200

    rails3 templates

--- rails3.rb
+++ rails3.rb
@@ -5,9 +5,9 @@ create_file 'Gemfile', <<-GEMFILE
 source 'http://rubygems.org'
 source 'http://gems.github.com'
 
-gem 'rails'
-gem 'sqlite3-ruby', :require => 'sqlite3'
-gem 'pg', :group => :production
+gem 'mysql2'
+gem 'mongrel'
+gem 'haml-rails'
 
 # Generators
 gem 'rails3-generators' # generators for non-rails-3-compatible apps 
@@ -15,32 +15,12 @@ gem 'nifty-generators'  # generators for non-rails-3-compatible apps
 
 # View Helpers
 gem 'jquery-rails'
-gem 'json'
-
-
-# Authentication & Authorization
-gem 'devise'
-gem 'cancan'
-
-# Uploads
-gem 'carrierwave' # simpler alternative to paperclip
-gem 'mini_magick' # simpler interface to RMagick
-gem 'paperclip'
-gem 'aws' # amazon storage
-  
-# Search
-gem 'thinking-sphinx', 
-  :require => 'thinking_sphinx', 
-  :git => "git://github.com/freelancing-god/thinking-sphinx.git", 
-  :branch => "rails3"  
-
-# Deploy
-gem 'capistrano'
 
 # Test
 group :test, :cucumber, :development do
   gem 'factory_girl' # replaces fixtures
   gem 'factory_girl_rails', '~> 1.0.0' # for rails 3
+  gem 'autotest'
 end
 
 group :test, :cucumber do
@@ -54,15 +34,10 @@ group :test, :cucumber do
   gem 'mocha' # library for mocking/stubbing
   gem 'webmock' # library for stubbing http requests
   gem 'spork'
-  gem "jasmine",
-    :git => "git://github.com/pivotal/jasmine-gem.git",
-    :branch => "rspec2-rails3",
-    :submodules => true
 end
 
 # Development tools
 group :development do
-  gem 'thin' # fast server
   gem 'wirble' # awesome irb
   gem 'awesome_print' # print ruby objects awesomely
 end
@@ -88,60 +63,17 @@ application generators
 # jQuery
 get "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js",  "public/javascripts/jquery.js"
 get "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.1/jquery-ui.min.js", "public/javascripts/jquery-ui.js"
-get "http://github.com/rails/jquery-ujs/raw/master/src/rails.js", "public/javascripts/rails.js"
-
+#get "http://github.com/rails/jquery-ujs/raw/master/src/rails.js", "public/javascripts/rails.js"
+get "https://github.com/rails/jquery-ujs/blob/master/src/rails.js", "public/javascripts/rails.js"
 gsub_file 'config/application.rb', 'config.action_view.javascript_expansions[:defaults] = %w()', 'config.action_view.javascript_expansions[:defaults] = %w(jquery.js jquery-ui.js rails.js)'
 
-# Mailer
-initializer 'mail.rb', <<-CODE
-ActionMailer::Base.smtp_settings = {
- :address => "localhost",
- :port => 1025,
- :domain => "#{app_name}.com"
-}
-CODE
-
 # Remove crap
-remove_file "README"
-remove_file "LICENSE"
 remove_file "public/index.html"
 remove_file "app/views/layouts/application.html.erb"
-remove_file "public/favicon.ico"
 remove_file "public/robots.txt"
-remove_file "public/images/rails.png"
-
-# Setup
-run 'capify .'
-run 'bundle install --without production'
 
 # Database and RSpec/Cucumber
 rake "db:migrate"
 generate "rspec:install"
 generate "cucumber:install --rspec --capybara"
 generate "pickle"
-
-# Git
-gitignore = <<-CODE
-.DS_Store
-log/*.log
-log/*.pid
-config/database.yml
-db/*.sqlite3
-rerun.txt
-public/system
-db/sphinx
-config/development.sphinx.conf
-.bundle
-tmp/**/*
-**/.DS_Store
-db/schema.rb
-vendor/cache/*
-CODE
-
-create_file ".gitignore", gitignore
-create_file "log/.gitkeep"
-create_file "tmp/.gitkeep"
-
-git :init
-git :add => "."
-git :commit => "-m 'Initial commit'"
